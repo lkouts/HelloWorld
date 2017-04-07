f1c0cfa2d4ed84457a0c7f7cc6d83a2874e6228b
Larry Kouts <lkoutsavlis@gmail.com>
2017-04-07 10:05:32 -0400

    Test

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Press any key to exit");
+            Console.WriteLine("Press any key to exit Change 2");
             var exit = Console.ReadLine();
 
             Console.WriteLine(exit);
