3bb6e025006424634a442a946fd4603a733b135e
Larry Kouts <lkoutsavlis@gmail.com>
2017-04-07 10:00:58 -0400

    for rev of Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit");
+            var exit = Console.ReadLine();
+
+            Console.WriteLine(exit);
+            Console.WriteLine("Console App will now close in 5 seconds");
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+}
